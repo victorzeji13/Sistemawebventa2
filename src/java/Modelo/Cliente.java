9a6404ab00b5a6bcc798f38cb76e4b1821ce42e4
Victor Zegarra jibaja <victor.zeji.2627@gmail.com>
2023-10-26 16:53:17 -0500

    Modulo empleado

---
+++ src/java/Modelo/Cliente.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+public class Cliente {
+    //inicializar variables
+    private int idCliente;
+    private String dni;
+    private String nombres;
+    private String direccion;
+    private String estado;
+
+    public Cliente() {
+        
+    }
+
+    public Cliente(int idCliente, String dni, String nombres, String direccion, String estado) {
+        //inicializar variables del constructor
+        this.idCliente = idCliente;
+        this.dni = dni;
+        this.nombres = nombres;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+
+    public Cliente(String dni, String nombres, String direccion, String estado) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+
+    /**
+     * @return the idCliente
+     */
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * @param idCliente the idCliente to set
+     */
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the nombres
+     */
+    public String getNombres() {
+        return nombres;
+    }
+
+    /**
+     * @param nombres the nombres to set
+     */
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+      
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+
+}
